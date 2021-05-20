9467b19908f3ef0555c1aeeaca9c7ddace398b89
Jblack017 <blackhamjoshua@comcast.net>
2021-05-20 00:26:41 -0600

    working comming

--- sketch.js
+++ sketch.js
@@ -1,43 +1,49 @@
-let canvasHeight = window.outerWidth;
-let canvasWidth = window.outerHeight;
+const canvasHeight = window.outerWidth;
+const canvasWidth = window.outerHeight;
+
+let r1 = 0;
+let r2 = 0;
+
+let a1 = 0;
+let a2 = 0;
+
+let a1Inc = 0;
+let a2Inc = 0;
+
+let x1 = 0;
+let y1 = 0;
 
 function setup() {
   createCanvas(canvasHeight, canvasWidth);
   angleMode(DEGREES);
   background(0);
-  frameRate(180);
+  frameRate(60);
 
-  r1 = random(100, 123);
-  r2 = random(100, 134);
+  r1 = random(100, 115);
+  r2 = random(120, 175);
 
-  a1Inc = random(100, 145);
-  a2Inc = random(100, 156);
-
-  prevX = random(100, 145);
-  prevY = random(100, 145);
-
-  a1 = random(100, 145);
-  a2 = random(100, 145);
-
-  a1Inc = random(100, 145);
-  a2Inc = random(100, 145);
+  a1Inc = random(0.1, 1);
+  a2Inc = random(0.1, 1);
 }
 
 function draw() {
   translate(mouseX, mouseY);
-  stroke(300);
 
   if (mouseIsPressed) {
-    let x1 = r1 * sin(a1);
-    let y1 = r1 * cos(a1);
+    let prevX = 0;
+    let prevY = 0;
 
-    let x2 = x1 + r2 * cos(a2);
-    let y2 = y1 + r2 * sin(a2);
+    for (let i = 0; i < 50; i++) {
+      i % 2 === 0 ? (x1 = r1 * sin(a1)) : (x1 = r1 * cos(a1));
+      i % 2 !== 0 ? (y1 = r1 * sin(a1)) : (y1 = r1 * cos(a1));
 
-    for (let i = 0; i < 3; i++) {
-      let r = map(sin(frameCount), -1, 1, 0, 150);
-      let g = map(sin(frameCount), -1, 1, 0, 50);
-      let b = map(sin(frameCount), -1, 1, 100, 255);
+      let x2 = x1 + r2 * sin(a2);
+      let y2 = y1 + r2 / cos(a2);
+
+      let r = map(cos(frameCount), -1, 1, random(80, 180), random(120, 220));
+      let g = map(cos(frameCount), -1, 1, random(10, 25), random(70, 95));
+      let b = map(cos(frameCount), -1, 1, random(7, 10), random(75, 80));
+
       stroke(r, g, b);
 
       line(prevX, prevY, x2, y2);
@@ -49,25 +55,29 @@ function draw() {
       a2 += a2Inc;
     }
   } else {
-    let x1 = r1 * cos(a1);
-    let y1 = r1 * sin(a1);
+    let prevX = 0;
+    let prevY = 0;
 
-    let x2 = x1 + r2 * cos(a2);
-    let y2 = y1 + r2 * sin(a2);
+    for (let i = 0; i < 690; i++) {
+      i % 2 === 0 ? (x1 = r1 * sin(a1)) : (x1 = r1 * sin(a1));
+      i % 2 !== 0 ? (y1 = r1 / cos(a1)) : (y1 = r1 / cos(a1));
 
-    for (let i = 0; i < 3; i++) {
-      let r = map(cos(frameCount - i), -1, 1, 20, 100);
-      let g = map(cos(frameCount - i), -1, 1, 100, 200);
-      let b = map(cos(frameCount - i), -1, 1, 100, 200);
+      let x2 = x1 - r2 * cos(a2);
+      let y2 = y1 - r2 * sin(a2);
+
+      let r = map(cos(frameCount), -1, 1, random(10, 25), random(70, 95));
+      let g = map(cos(frameCount), -1, 1, random(7, 10), random(75, 80));
+      let b = map(cos(frameCount), -1, 1, random(80, 175), random(180, 200));
+
       stroke(r, g, b);
+
+      prevX = x2 + 1;
+      prevY = y2 + 1;
 
       line(prevX, prevY, x2, y2);
 
-      prevX = x2 + 1;
-      prevY = y2 + 2;
-
-      a1 -= a1Inc;
-      a2 -= a2Inc;
+      a1 += a1Inc;
+      a2 += a2Inc;
     }
   }
 }
